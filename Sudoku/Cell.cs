f7cf9aa2125981c972b336960f8bc960e5474ea0
Anastasiia <lnl2323232323@gmail.com>
2022-05-12 18:32:57 +0300

    cell

---
+++ Sudoku/Cell.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows.Forms;
+
+namespace Sudoku
+{
+    public class Cell:Button
+    {
+        private int x;
+        private int y;
+        private bool isLocked;
+        private int value;
+        public int X
+        {
+            get => x;
+            set => x = value;
+        }
+        public int Y
+        {
+            get => y;
+            set => y = value;
+        }
+        public bool IsLocked
+        {
+            get => isLocked;
+            set => isLocked = value;
+        }
+        public int Value
+        {
+            get => value;
+            set => this.value = value;
+        }
+        public void Clear()
+        {
+            this.Text = string.Empty;
+            this.IsLocked = false;
+        }
+    }
+}
